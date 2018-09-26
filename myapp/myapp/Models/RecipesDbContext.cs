c1ac0b01a1a0e5be697df9a9db8eef856c0f70e8
Wilson Nie <wilson.nie13@gmail.com>
2018-09-26 15:10:01 -0700

    inital commit creating web appliaction using razor pages

---
+++ myapp/myapp/Models/RecipesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace myapp.Models
+{
+    public class RecipesDbContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public RecipesDbContext(DbContextOptions<RecipesDbContext> options)
+            : base(options)
+        {
+            this.EnsureSeedData();
+        }
+        
+    }
+}
